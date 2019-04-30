SusannaMaria/eduke32-1

commit 55dca6df8cd6b381dd4440768d3f9ae8bdcd4347

Remove enabling/disabling of deprecated GL_TEXTURE_2D in Polymost. For Polymer, temporarily workaround coupling with Polymost and usage of the fixed-function pipeline. Additionally, fix typo in polymost_printtext256().
git-svn-id: https://svn.eduke32.com/eduke32@7620 1a8010ca-5511-0410-912e-c29ae57300e0

source/build/src/polymer.cpp
@@ -930,9 +930,6 @@ void                polymer_glinit(void)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
     glViewport(windowxy1.x, ydim-(windowxy2.y+1),windowxy2.x-windowxy1.x+1, windowxy2.y-windowxy1.y+1);
 
-    // texturing
-    glEnable(GL_TEXTURE_2D);
-
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LEQUAL);
 
@@ -1059,6 +1056,23 @@ void                polymer_loadboard(void)
     polymer_resetlights();
 
     if (pr_verbosity >= 1 && numsectors) OSD_Printf("PR : Board loaded.\n");
+}
+
+int32_t polymer_printtext256(int32_t xpos, int32_t ypos, int16_t col, int16_t backcol, const char *name, char fontsize)
+{
+    //POGOTODO: Polymer should implement this so it's no longer coupled with Polymost & reliant on the fixed-function pipeline
+    glEnable(GL_TEXTURE_2D);
+    int32_t returnVal = polymost_printtext256(xpos, ypos, col, backcol, name, fontsize);
+    glDisable(GL_TEXTURE_2D);
+    return returnVal;
+}
+
+void polymer_fillpolygon(int32_t npoints)
+{
+    //POGOTODO: Polymer should implement this so it's no longer coupled with Polymost & reliant on the fixed-function pipeline
+    glEnable(GL_TEXTURE_2D);
+    polymost_fillpolygon(npoints);
+    glDisable(GL_TEXTURE_2D);
 }
 
 // The parallaxed ART sky angle divisor corresponding to a horizfrac of 32768.
@@ -4217,6 +4231,7 @@ static void         polymer_drawartsky(int16_t tilenum, char palnum, int8_t shad
         i++;
     }
 
+    glEnable(GL_TEXTURE_2D);
     i = 0;
     j = 8;  // In Polymer, an ART sky has always 8 sides...
     while (i < j)
@@ -4237,6 +4252,7 @@ static void         polymer_drawartsky(int16_t tilenum, char palnum, int8_t shad
 
         i++;
     }
+    glDisable(GL_TEXTURE_2D);
 }
 
 static void         polymer_drawartskyquad(int32_t p1, int32_t p2, GLfloat height)

source/build/include/polymer.h
@@ -352,6 +352,8 @@ void                polymer_setaspect(int32_t);
 void                polymer_glinit(void);
 void                polymer_resetlights(void);
 void                polymer_loadboard(void);
+int32_t             polymer_printtext256(int32_t xpos, int32_t ypos, int16_t col, int16_t backcol, const char *name, char fontsize);
+void                polymer_fillpolygon(int32_t npoints);
 void polymer_drawrooms(int32_t daposx, int32_t daposy, int32_t daposz, fix16_t daang, int32_t dahoriz, int16_t dacursectnum);
 void                polymer_drawmasks(void);
 void                polymer_editorpick(void);

source/build/include/polymost.h
@@ -83,7 +83,7 @@ enum {
 
 void gltexinvalidate(int32_t dapicnum, int32_t dapalnum, int32_t dameth);
 void gltexinvalidatetype(int32_t type);
-int32_t polymost_printext256(int32_t xpos, int32_t ypos, int16_t col, int16_t backcol, const char *name, char fontsize);
+int32_t polymost_printtext256(int32_t xpos, int32_t ypos, int16_t col, int16_t backcol, const char *name, char fontsize);
 
 extern float curpolygonoffset;
 

source/build/src/2d.cpp
@@ -155,8 +155,6 @@ static void drawlinegl(int32_t x1, int32_t y1, int32_t x2, int32_t y2, palette_t
     {
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-
-        glDisable(GL_TEXTURE_2D);
     }
 
     gloy1 = -1;
@@ -1428,8 +1426,6 @@ void polymostSet2dView(void)
     {
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-
-        glDisable(GL_TEXTURE_2D);
     }
 
     gloy1 = -1;

source/build/src/animvpx.cpp
@@ -457,7 +457,6 @@ void animvpx_setup_glstate(int32_t animvpx_flags)
     glDisable(GL_DEPTH_TEST);
     glDisable(GL_BLEND);
     glDisable(GL_CULL_FACE);
-    glEnable(GL_TEXTURE_2D);
 
 #ifdef USE_GLEXT
     glActiveTexture(GL_TEXTURE0);

source/build/src/engine.cpp
@@ -5964,7 +5964,13 @@ static void renderFillPolygon(int32_t npoints)
 #ifdef USE_OPENGL
     if (videoGetRenderMode() >= REND_POLYMOST && in3dmode())
     {
-        polymost_fillpolygon(npoints);
+#ifdef POLYMER
+        if (videoGetRenderMode() == REND_POLYMER)
+            polymer_fillpolygon(npoints);
+        else
+#endif
+            polymost_fillpolygon(npoints);
+
         return;
     }
 #endif
@@ -12255,7 +12261,14 @@ void printext256(int32_t xpos, int32_t ypos, int16_t col, int16_t backcol, const
     else { fontptr = textfont; charxsiz = 8; }
 
 #ifdef USE_OPENGL
-    if (!polymost_printext256(xpos,ypos,col,backcol,name,fontsize)) return;
+#ifdef POLYMER
+    if (videoGetRenderMode() == REND_POLYMER)
+    {
+        if (!polymer_printtext256(xpos,ypos,col,backcol,name,fontsize)) return;
+    }
+    else
+#endif
+        if (!polymost_printtext256(xpos,ypos,col,backcol,name,fontsize)) return;
 # if 0
     if (videoGetRenderMode() >= REND_POLYMOST && in3dmode())
     {

source/build/src/mdsprite.cpp
@@ -2225,8 +2225,6 @@ static int32_t polymost_md3draw(md3model_t *m, tspriteptr_t tspr)
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
 
-    glEnable(GL_TEXTURE_2D);
-
     // tinting
     pc[0] = pc[1] = pc[2] = ((float)numshades - min(max((globalshade * shadescale) + m->shadeoff, 0.f), (float)numshades)) / (float)numshades;
     polytintflags_t const tintflags = hictinting[globalpal].f;

source/build/src/palette.cpp
@@ -58,7 +58,6 @@ void fullscreen_tint_gl(uint8_t r, uint8_t g, uint8_t b, uint8_t f)
 
     glDisable(GL_DEPTH_TEST);
     glDisable(GL_ALPHA_TEST);
-    glDisable(GL_TEXTURE_2D);
     polymost_setFogEnabled(false);
 
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);

commit e4405726f665be9bafdbc167e5b3d42c0d302450

Fix Polymer skybox bug introduced in r7620
git-svn-id: https://svn.eduke32.com/eduke32@7622 1a8010ca-5511-0410-912e-c29ae57300e0

source/build/src/polymer.cpp
@@ -4325,6 +4325,7 @@ static void         polymer_drawskybox(int16_t tilenum, char palnum, int8_t shad
         }
 
         glColor4f(color[0], color[1], color[2], 1.0);
+        glEnable(GL_TEXTURE_2D);
         glBindTexture(GL_TEXTURE_2D, pth ? pth->glpic : 0);
         if (pr_vbos > 0)
         {
@@ -4335,6 +4336,7 @@ static void         polymer_drawskybox(int16_t tilenum, char palnum, int8_t shad
             glTexCoordPointer(2, GL_FLOAT, 5 * sizeof(GLfloat), &skyboxdata[3 + (4 * 5 * i)]);
         }
         glDrawArrays(GL_QUADS, 0, 4);
+        glDisable(GL_TEXTURE_2D);
 
         i++;
     }